354541db9bab4c8b8c8dc824858fefbf3a56983c
Sergey3797 <anufrienko3797@yandex.ru>
2025-02-05 01:40:37 +0300

    Add files via upload

---
+++ synergy.py
@@ -0,0 +1,29 @@
+#Задание №1
+#Программа для запроса данных о питомце перед амбулаторным лечением. 
+print("Вид питомца") 
+a1 = input()
+print("Кличка")
+a2 = input()
+print("Возраст")
+a3 = input()
+print(a1,a2,a3)
+#Задание №2
+# Задание по биологии для студентов, В настоящее время выделяют 
+# следующие основные этапы эволюции человека:дриопитек(Dryopithecus) – рамапитек(Sivapithecus) – 
+# австралопитек(Australopithecus) – человек умелый(Homo habilis) 
+# – человек прямоходящий(Homo erectus) – человек разумный(Homo sapiens sapiens).
+#Присвойте каждому этапу на латыни этап на русском языке.
+print("Напишите стадии развития человека по порядку")
+print("Dryopithecus")
+a1 = input()
+print("Sivapithecus")
+a2 = input()
+print("Australopithecus")
+a3 = input()
+print("Homo habilis")
+a4 = input()
+print("Homo erectus")
+a5 = input()
+print("Homo neanderthalensis")
+a6 = input()
+print(a1, a2, a3, a3, a4, a5, a6, sep='=>')
